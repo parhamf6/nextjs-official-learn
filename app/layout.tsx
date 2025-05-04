6ac351ddf72eecebe3e591ca4513770f43b67d5a
parhamf6 <p.arhamforati838@gmail.com>
2025-05-04 14:12:39 +0330

    initial commit - added nextjs dashborad to github

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,22 @@
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts';
+
+
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" >
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        <main className="p-2 m-2 ml-4 mr-4 ">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
